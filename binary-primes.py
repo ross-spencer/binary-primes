d4c2911c944e20c82e02fc219a6ee5e7429495c3
ross-spencer <all.along.the.watchtower2001+github@gmail.com>
2012-12-17 18:48:57 +0000

    [bugfix] Whoops. Updated args documentation.

--- binary-primes.py
+++ binary-primes.py
@@ -1,7 +1,7 @@
 import argparse
 
 def parseCommandLine():
-	parser = argparse.ArgumentParser(description='Tool for the automated generation of digital objects based on the digital signatures documented in the PRONOM database maintained by The National Archives, UK.')
+	parser = argparse.ArgumentParser(description='Generate visual binary representations of prime numbers.')
 	parser.add_argument('--version', help="Display the version number of the tool", action='version', version='%(prog)s v0.1-BETA')
 	args = parser.parse_args()
 
